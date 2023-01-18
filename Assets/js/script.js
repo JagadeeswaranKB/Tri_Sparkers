7ac5bc7aaa1e554a7436945637c962e24573c28c
Jagadeeswaran K B <110410714+JagadeeswaranKB@users.noreply.github.com>
2023-01-18 20:22:11 +0530

    Update script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,7 +13,7 @@ hamburger .addEventListener('click', ()=>{
 var messages = [], //array that hold the record of each string in chat
   lastUserMessage = "", //keeps track of the most recent input string from the user
   botMessage = "", //var keeps track of what the chatbot is going to say
-  botName = 'Chatbot', //name of the chatbot
+  botName = 'Tri Sparkers', //name of the chatbot
   talking = true; //when false the speach function doesn't work
 //
 //
